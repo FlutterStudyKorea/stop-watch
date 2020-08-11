b39b0c2b2b6e7a82a8dfec73def63b53e302f414
Bentley Park <junggil.park@sovoro.kr>
2020-08-11 14:12:22 +0900

    stopwatchscreen dart 추가

---
+++ stopwatch/lib/stop_watch_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StopWatchScreen extends StatefulWidget {
+  final String title;
+
+  StopWatchScreen({this.title});
+
+  _StopWatchScreenState createState() => _StopWatchScreenState();
+}
+
+class _StopWatchScreenState extends State<StopWatchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
